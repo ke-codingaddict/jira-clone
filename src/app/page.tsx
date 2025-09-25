218f89fea9a16b794307d8839cbc0f49553f6137
Logger Key <loggerkey005@gmail.com>
2025-09-25 17:51:41 +0300

    login_button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useCurrent } from "./api/use.current";
 import { useRouter } from "next/navigation";
 import { useLogout } from "@/features/auth/api/use-logout";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
   const router = useRouter();
@@ -16,9 +17,10 @@ export default function Home() {
     }
   }, [data]);
   return (
-    <div className="flex items-center justify-center h-screen font-bold  flex-col gap-5">
+    <div className="flex items-center justify-center h-screen font-bold  flex-col gap-5 mx-auto text-center">
       Only visible to authorized users
-      <Button onClick={()=>mutate()}>Logout</Button>
+      <Button onClick={() => mutate()}>Logout</Button>
+      <UserButton />
     </div>
   );
 }

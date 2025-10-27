cd94220590398f05948d7949496a8a0dc225aade
Logger Key <loggerkey005@gmail.com>
2025-10-27 17:37:35 +0300

    task_settings

---
+++ src/features/tasks/api/use-delete-task.ts
@@ -0,0 +1,40 @@
+import { client } from "@/lib/rpc";
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.tasks)[":taskId"]["$delete"],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.tasks)[":taskId"]["$delete"]
+>;
+
+export const useDeleteTask = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param }) => {
+      const response = await client.api.tasks[":taskId"]["$delete"]({
+        param
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete task");
+      }
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success("Task deleted");
+      router.refresh();
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      queryClient.invalidateQueries({ queryKey: ["tasks", data] });
+    },
+    onError: () => {
+      toast.error("Failed to delete task");
+    }
+  });
+  return mutation;
+};
+1;
